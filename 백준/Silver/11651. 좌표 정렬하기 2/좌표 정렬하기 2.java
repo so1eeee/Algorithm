ce9147bdc684a1b0516f913766a174f77ad5d862
sol <120920408+so1eeee@users.noreply.github.com>
2025-06-02 14:12:12 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1724 ms, Memory: 194548 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+class Point {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        List<Point> pointList = new ArrayList<>();
+        for(int i = 0; i < N; i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            pointList.add(new Point(x, y));
+        }
+        Collections.sort(pointList, (a, b) -> {
+            if (a.y != b.y) {
+                return Integer.compare(a.y, b.y);
+            } else {
+                return Integer.compare(a.x, b.x);
+            }
+        });
+
+        for(Point point : pointList){
+            System.out.println(point.x + " " + point.y);
+        }
+    }
+}
