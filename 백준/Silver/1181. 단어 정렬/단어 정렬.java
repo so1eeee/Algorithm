9433e19d1d1746de97d7ebeefc4026804d20c614
sol <120920408+so1eeee@users.noreply.github.com>
2025-06-02 13:18:31 +0900

    [Silver V] Title: 단어 정렬, Time: 724 ms, Memory: 37304 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        Set<String> wordSet = new HashSet<>();
+        for(int i = 0; i < n; i++){
+            String word = sc.next();
+            wordSet.add(word);
+        }
+
+        List<String> wordList = new ArrayList<>(wordSet);
+
+        Collections.sort(wordList, (a, b) -> {
+            if (a.length() == b.length()) {
+                return a.compareTo(b);
+            } else {
+                return Integer.compare(a.length(), b.length());
+            }
+        });
+
+        for(String word : wordList) {
+            System.out.println(word);
+        }
+    }
+}
