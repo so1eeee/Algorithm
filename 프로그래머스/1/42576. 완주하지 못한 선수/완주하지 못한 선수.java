1c65f181ccff43a8f9dc4230bbcf52bcfbe4e642
sol <120920408+so1eeee@users.noreply.github.com>
2025-06-20 03:25:29 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 77.81 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,13 +2,10 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        
         HashMap<String, Integer> hashMap = new HashMap<>();
-        
         for(String string : completion){
             hashMap.put(string, hashMap.getOrDefault(string, 0) + 1);
         }
-        
         for(String string : participant){
             if(hashMap.getOrDefault(string, 0) == 0){
                 return string;
