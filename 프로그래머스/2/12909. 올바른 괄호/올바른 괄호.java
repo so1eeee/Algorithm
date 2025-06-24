6c328a421d934c6a385cb8fd9cf14563d5f85bc6
sol <120920408+so1eeee@users.noreply.github.com>
2025-06-24 22:45:37 +0900

    [level 2] Title: 올바른 괄호, Time: 15.24 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,20 +2,20 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        
+
         ArrayDeque<Character> deque = new ArrayDeque<>();
-        
+
         char[] a = s.toCharArray();
-        
+
         for(char c : a){
             if(c == '('){
                 deque.offerFirst(c);
             }
             else{
-                 if(deque.isEmpty()) {
+                if(deque.isEmpty()) {
                     return false;
                 }
-                deque.pollFirst();
+                deque.poll();
             }
         }
 
