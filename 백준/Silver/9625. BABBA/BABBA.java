95c65a30d0fbf293e8f83c71bddda7f15b9bf308
sol <120920408+so1eeee@users.noreply.github.com>
2025-10-19 11:05:02 +0900

    [Silver V] Title: BABBA, Time: 116 ms, Memory: 16044 KB -BaekjoonHub

---
+++ 백준/Silver/9625. BABBA/BABBA.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int K = Integer.parseInt(br.readLine());
+		int[] dpA = new int[K + 1];
+		int[] dpB = new int[K + 1];
+		dpA[0] = 1;
+		dpB[0] = 0;
+		for (int i = 1; i <= K ; i++) {
+			dpA[i] = dpB[i - 1];
+			dpB[i] = dpA[i - 1] + dpB[i - 1];
+		}
+		System.out.println(dpA[K] + " " + dpB[K]);
+	}
+}
