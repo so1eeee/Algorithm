f2711249a6f5d114ba3ad4b344b28ca5a96d6e8f
sol <120920408+so1eeee@users.noreply.github.com>
2025-11-03 16:17:18 +0900

    [Silver III] Title: 주유소, Time: 372 ms, Memory: 36068 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -7,24 +7,24 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] dist = new int[N - 1];
-		int[] cost = new int[N];
+		long[] dist = new long[N - 1];
+		long[] cost = new long[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N - 1; i++) {
-			dist[i] = Integer.parseInt(st.nextToken());
+			dist[i] = Long.parseLong(st.nextToken());
 		}
 		StringTokenizer st2 = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			cost[i] = Integer.parseInt(st2.nextToken());
+			cost[i] = Long.parseLong(st2.nextToken());
 		}
-		int minCost = cost[0];
-		int sumCost = 0;
+		long minCost = cost[0];
+		long totalCost = 0;
 		for (int i = 0; i < N - 1; i++) {
 			if (cost[i] < minCost) {
 				minCost = cost[i];
 			}
-			sumCost += minCost * dist[i];
+			totalCost += minCost * dist[i];
 		}
-		System.out.println(sumCost);
+		System.out.println(totalCost);
 	}
 }
