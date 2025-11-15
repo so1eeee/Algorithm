e86f3a0dcc47864ab7466b87e2544bc07815d40f
sol <120920408+so1eeee@users.noreply.github.com>
2025-11-16 01:49:10 +0900

    [Silver I] Title: 단지번호붙이기, Time: 104 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,61 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Queue;
 
 public class Main {
-    static int N;
-    static int[][] map;
-    static boolean[][] visited;
-    static int[] dx = {0, 0, 1, -1};
-    static int[] dy = {1, -1, 0, 0};
+	static int N;
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] dx = {0, 0, 1, -1};
+	static int[] dy = {1, -1, 0, 0};
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        sc.nextLine();
+	static class Point {
+		int x;
+		int y;
 
-        map = new int[N][N];
-        visited = new boolean[N][N];
+		Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 
-        for(int i = 0; i < N; i++){
-            String line = sc.nextLine();
-            for(int j = 0; j < N; j++){
-                map[i][j] = line.charAt(j) - '0';
-            }
-        }
+	static int bfs(int x, int y) {
+		Queue<Point> q = new ArrayDeque<>();
+		q.offer(new Point(x, y));
+		visited[x][y] = true;
+		int cnt = 1;
+		while(!q.isEmpty()){
+			Point cur = q.poll();
+			for (int i = 0; i < 4; i++) {
+				int nx = cur.x + dx[i];
+				int ny = cur.y + dy[i];
+				if(nx >= 0 && nx < N && ny >= 0 && ny < N){
+					if(map[nx][ny] == 1 && !visited[nx][ny]){
+						visited[nx][ny] = true;
+						q.offer(new Point(nx, ny));
+						cnt++;
+					}
+				}
+			}
+		}
+		return cnt;
+	}
 
-        List<Integer> complexes = new ArrayList<>();
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if(map[i][j] == 1 && !visited[i][j]){
-                    int houseCount = dfs(i, j);
-                    complexes.add(houseCount);
-                }
-            }
-        }
-
-        Collections.sort(complexes);
-
-        System.out.println(complexes.size());
-        for (int count : complexes) {
-            System.out.println(count);
-        }
-    }
-
-    private static int dfs(int x, int y) {
-        visited[x][y] = true;
-        int count = 1;
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(nx >= 0 && nx < N && ny >= 0 && ny < N){
-                if(map[nx][ny] == 1 && !visited[nx][ny]){
-                    count += dfs(nx, ny);
-                }
-            }
-        }
-        return count;
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		visited = new boolean[N][N];
+		for(int i = 0; i < N; i++){
+			String line = br.readLine();
+			for(int j = 0; j < N; j++){
+				map[i][j] = line.charAt(j) - '0';
+			}
+		}
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if(map[i][j] == 1 && !visited[i][j]){
+					list.add(bfs(i, j));
+				}
+			}
+		}
+		Collections.sort(list);
+		System.out.println(list.size());
+		for (int cnt : list) {
+			System.out.println(cnt);
+		}
+	}
 }
