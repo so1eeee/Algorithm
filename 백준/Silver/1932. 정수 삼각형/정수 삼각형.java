de871648a2779508a533cfb934fb9d740ed279e4
sol <120920408+so1eeee@users.noreply.github.com>
2025-11-15 19:31:13 +0900

    [Silver I] Title: 정수 삼각형, Time: 228 ms, Memory: 27112 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -8,26 +8,24 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int[][] arr = new int[n][n];
-		int[][][] dp = new int[n][n][2];
+		int[][] dp = new int[n][n];
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j <= i; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		dp[0][0][0] = arr[0][0];
-		dp[0][0][1] = arr[0][0];
+		dp[0][0] = arr[0][0];
 		for (int i = 1; i < n; i++) {
-			for (int j = 0; j <= i; j++) {
-				dp[i][j][0] = Math.max(dp[i - 1][j][0], dp[i - 1][j][1]) + arr[i][j];
-				dp[i][j][1] = j > 0 ? Math.max(dp[i - 1][j - 1][0], dp[i - 1][j - 1][1]) + arr[i][j] : Math.max(dp[i - 1][j][0], dp[i - 1][j][1]) + arr[i][j];
+			dp[i][0] = dp[i - 1][0] + arr[i][0];
+			for (int j = 1; j < i; j++) {
+				dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + arr[i][j];
 			}
+			dp[i][i] = dp[i - 1][i - 1] + arr[i][i];
 		}
 		int max = 0;
 		for (int j = 0; j < n; j++) {
-			for (int dir = 0; dir < 2; dir++) {
-				max = Math.max(max, dp[n - 1][j][dir]);
-			}
+			max = Math.max(max, dp[n - 1][j]);
 		}
 		System.out.println(max);
 	}
