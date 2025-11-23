66745d01f6f2e8476830ed2404729863d1159a0a
sol <120920408+so1eeee@users.noreply.github.com>
2025-11-23 17:46:51 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long a, b, c;
+long long fun(long long a, long long b)
+{
+    if (b == 1)
+        return a % c;
+    long long ret = fun(a, b / 2);
+    ret = (ret * ret) % c;
+    if (b % 2)
+        ret = (ret * a) % c;
+    return ret;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> a >> b >> c;
+    cout << fun(a, b) << "\n";
+    return 0;
+}
