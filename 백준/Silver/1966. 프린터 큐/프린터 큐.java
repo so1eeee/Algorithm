a3ed08187354fb80fdd0c681fdd641dfc8ac4f1d
sol <120920408+so1eeee@users.noreply.github.com>
2025-12-01 20:14:04 +0900

    [Silver III] Title: 프린터 큐, Time: 128 ms, Memory: 14752 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,58 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.Deque;
-import java.util.Scanner;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static class Docs {
-        int index;
+	static class Node {
+        int idx;
         int priority;
 
-        Docs(int index, int priority) {
-            this.index = index;
+        public Node(int idx, int priority) {
+            this.idx = idx;
             this.priority = priority;
         }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int testCase = sc.nextInt();
-
-        for(int i = 0; i < testCase; i++) {
-            int n = sc.nextInt(); // 문서의 개수
-            int m = sc.nextInt(); // 궁금한 문서 위치
-
-            Deque<Docs> deque = new ArrayDeque<>();
-
-            for (int j = 0; j < n; j++) {
-                int priority = sc.nextInt();
-                deque.offerLast(new Docs(j, priority));
-            }
-
-            int printOrder = 0;
-
-            while (!deque.isEmpty()) {
-                Docs cur = deque.pollFirst();
-
-                boolean hasHigher = false;
-                for (Docs doc : deque) {
-                    if (doc.priority > cur.priority) {
-                        hasHigher = true;
-                        break;
-                    }
-                }
-
-                if (hasHigher) {
-                    deque.offerLast(cur);
-                } else {
-                    printOrder++;
-                    if (cur.index == m) {
-                        System.out.println(printOrder);
-                        break;
-                    }
-                }
-            }
-            
-        }
+    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		while (T-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			Deque<Node> deque = new ArrayDeque<>();
+			PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				int priority = Integer.parseInt(st.nextToken());
+				deque.offer(new Node(j, priority));
+				pq.offer(priority);
+			}
+			int cnt = 1;
+			while (!deque.isEmpty()) {
+				Node cur = deque.peek();
+				if (!pq.isEmpty() && cur.priority < pq.peek()) {
+					deque.offer(deque.poll());
+				} else if (!pq.isEmpty() && cur.priority == pq.peek()) {
+					if (cur.idx == M) {
+						System.out.println(cnt);
+						break;
+					}
+					pq.poll();
+					deque.poll();
+					cnt++;
+				}
+			}
+		}
     }
 }
