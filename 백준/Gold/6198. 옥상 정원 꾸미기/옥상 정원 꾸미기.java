bb41bb376a6d3e8302942f8adcecdfbd182b14f2
sol <120920408+so1eeee@users.noreply.github.com>
2025-12-10 10:00:59 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 236 ms, Memory: 24352 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -2,30 +2,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Deque;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-		int[] result = new int[N];
-		Arrays.fill(result, N);
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
 		Deque<Integer> deque = new ArrayDeque<>();
-		for (int i = 0; i < N; i++) {
-			while (!deque.isEmpty() && arr[i] >= arr[deque.peekLast()]) {
-				int idx = deque.pollLast();
-				result[idx] = i;
-			}
-			deque.offer(i);
-		}
 		long sum = 0;
 		for (int i = 0; i < N; i++) {
-			sum += (result[i] - i - 1);
+			int num = Integer.parseInt(br.readLine());
+			while (!deque.isEmpty() && num >= deque.peekLast()) {
+				deque.pollLast();
+			}
+			sum += deque.size();
+			deque.offer(num);
 		}
 		System.out.println(sum);
 	}
