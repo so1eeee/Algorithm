1b2e1d79b37486429ca66fd7ded398f64fcc7973
sol <120920408+so1eeee@users.noreply.github.com>
2025-12-13 14:30:46 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2260 ms, Memory: 331328 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Edge {
+	int start;
+	int end;
+	int weight;
+
+	public Edge(int start, int end, int weight) {
+		this.start = start;
+		this.end = end;
+		this.weight = weight;
+	}
+}
+
+public class Main {
+	static int[] parent;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		Edge[] edges = new Edge[M];
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			edges[i] = new Edge(a, b, c);
+		}
+		Arrays.sort(edges, (e1, e2) -> e1.weight - e2.weight);
+		parent = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			parent[i] = i;
+		}
+		int result = 0;
+		int max = 0;
+		for (Edge edge : edges) {
+			if (find(edge.start) != find(edge.end)) {
+				union(edge.start, edge.end);
+				max = Math.max(max, edge.weight);
+				result += edge.weight;
+			}
+		}
+		System.out.println(result - max);
+	}
+
+	static int find(int x) {
+		if (parent[x] == x) {
+			return x;
+		}
+		return parent[x] = find(parent[x]);
+	}
+
+	static void union(int a, int b) {
+		int rootA = find(a);
+		int rootB = find(b);
+		if (rootA != rootB) {
+			parent[rootB] = rootA;
+		}
+	}
+}
